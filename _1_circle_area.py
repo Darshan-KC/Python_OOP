d6a72d68d5a78be39dd01ab22e66d7d8415a5e42
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-24 20:03:09 +0545

    progressing

--- _1_circle_area.py
+++ _1_circle_area.py
@@ -1,1 +1,10 @@
 #  Write a Python program to create a class representing a Circle. Include methods to calculate its area and perimeter.
+class Circle:
+    
+    def __init__(self,radius) -> None:
+        self.radius = radius 
+        
+    def area(self):
+        print("The area of circle is {0}",self.radius * self.radius)
+        
+    

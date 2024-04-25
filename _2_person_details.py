1eef8a8f5fa967a46c9539f70efc338727fa76bb
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-25 21:31:40 +0545

    Closes #1: Issue solved

--- _2_person_details.py
+++ _2_person_details.py
@@ -1,1 +1,18 @@
 # Write a Python program to create a person class. Include attributes like name, country and date of birth. Implement a method to determine the person's age.
+import datetime
+
+class Person:
+    def __init__(self,name,country,dob) -> None:
+        self.name = name
+        self.country = country
+        self.dob = dob 
+    
+    def date_of_birth(self):
+        dob = datetime.datetime.strptime(self.dob,"%Y/%m/%d")
+        current_date = datetime.datetime.now()
+        age = current_date.year - dob.year
+        print("Age of a {} is {}".format(self.name,age))
+        
+obj = Person("Hari","Nepal","2000/01/01")
+obj.date_of_birth()
+    

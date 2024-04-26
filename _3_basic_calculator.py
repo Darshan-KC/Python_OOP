cb97339849c389f57a8913ac8ab1e66a75f0c4c7
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-26 06:53:59 +0545

    progressing

--- _3_basic_calculator.py
+++ _3_basic_calculator.py
@@ -1,1 +1,5 @@
 # Write a Python program to create a calculator class. Include methods for basic arithmetic operations.
+class Calculator:
+    def __init__(self,num1,num2) -> None:
+        self.num1 = num1
+        self.num2 = num2

ac1a2aa4d6e0a5e72b5e90e686e438a7b6224b80
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-27 20:28:50 +0545

    3 basic calculator complete

--- _3_basic_calculator.py
+++ _3_basic_calculator.py
@@ -5,18 +5,50 @@ class Calculator:
         self.num2 = num2
         
     def sum(self):
-        return self.num1 + self.num2
+        print("The sum of {} and {} is {}".format(self.num1,self.num2, (self.num1 + self.num2)))
     
     def difference(self):
-        return self.num1 - self.num2 
+        print("The difference of {} from {} is {}".format(self.num1,self.num2, (self.num1 - self.num2))) 
     
     def product(self):
-        return self.num1 * self.num2 
+        print("The product of {} and {} is {}".format(self.num1,self.num2, (self.num1 * self.num2))) 
     
     def divison(self):
-        return self.num1 / self.num2
+        print("The division of {} and {} is {}".format(self.num1,self.num2, (self.num1 / self.num2)))
     
     def remainder(self):
-        return self.num1 % self.num2 
+        print("The remainder {} and {} is {}".format(self.num1,self.num2, (self.num1 % self.num2))) 
+
+
+def numcheck(num)->eval:
+    try:
+        n = eval(num)
+    except:
+        print("Invalid type")
+        tem = input("Enter the valid number: ")
+        n = numcheck(tem)
     
+    return n
     
+num1 = input("Enter the first number: ")
+num1 = numcheck(num1)
+num2 = input("Enter the second number: ")
+num2 = numcheck(num2)
+op = input("Enter the operator: ")
+
+while(not((op == "+") or (op == "-") or (op == '*') or (op == '/') or (op == '%'))):
+    print("The operator is ", op)
+    print("\n!!!\tThe operator must be one of ('+','-','*','/','%')!!!")
+    op = input("Enter the correct operator: ")
+
+cal = Calculator(num1,num2)
+if(op == '+'):
+    cal.sum()
+elif op == '-':
+    cal.difference()
+elif op == '*':
+    cal.product()
+elif op == '/':
+    cal.divison()
+else:
+    cal.remainder()

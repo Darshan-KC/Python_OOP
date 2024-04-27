e09343f0e9521457e48d24e92a11dc8fa32bda8c
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-27 19:32:28 +0545

    progress

--- _3_basic_calculator.py
+++ _3_basic_calculator.py
@@ -3,3 +3,20 @@ class Calculator:
     def __init__(self,num1,num2) -> None:
         self.num1 = num1
         self.num2 = num2
+        
+    def sum(self):
+        return self.num1 + self.num2
+    
+    def difference(self):
+        return self.num1 - self.num2 
+    
+    def product(self):
+        return self.num1 * self.num2 
+    
+    def divison(self):
+        return self.num1 / self.num2
+    
+    def remainder(self):
+        return self.num1 % self.num2 
+    
+    

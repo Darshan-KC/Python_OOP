c3ca5e2042ee39b917f328e0000d7d20b4a4bb2c
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-28 20:53:25 +0545

    4 shape added

---
+++ _4_shape.py
@@ -0,0 +1,1 @@
+# Write a Python program to create a class that represents a shape. Include methods to calculate its area and perimeter. Implement subclasses for different shapes like circle, triangle, and square.

b9c00337d4603fbd14b675969a230958cc2bbb6c
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-30 20:13:43 +0545

    5 super function added

---
+++ _5_super_function.py
@@ -0,0 +1,10 @@
+# Using the super() function. Define a base class Animal and derived class Dog 
+class Animal:
+    def __init__(self,name) -> None:
+        self.name = name
+        
+    def speak(ram)-> str: 
+        return f"{ram.name} say hello"
+    
+ram = Animal("Ram")
+print(ram.speak())

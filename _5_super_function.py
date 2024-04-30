302ba5331c8f88921ea9a8ae5e10478b8f890016
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-30 20:16:36 +0545

    5 super function complete

--- _5_super_function.py
+++ _5_super_function.py
@@ -6,5 +6,15 @@ class Animal:
     def speak(ram)-> str: 
         return f"{ram.name} say hello"
     
+class Dog(Animal):
+    def __init__(self, name) -> None:
+        super().__init__(name)
+        
+    def speak(self) -> str:
+        return f"{self.name} barks"
+    
 ram = Animal("Ram")
 print(ram.speak())
+
+dog = Dog("Lucy")
+print(dog.speak())

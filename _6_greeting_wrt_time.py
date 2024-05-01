c0859b102102f35aa0d730aa1f21e2a4be4c0117
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-01 21:11:16 +0545

    6 greeting with respect to time complete

--- _6_greeting_wrt_time.py
+++ _6_greeting_wrt_time.py
@@ -2,7 +2,7 @@
 import time
 class Greet:
     def __init__(self,name) -> None:
-        self.name
+        self.name = name
         
     def goodMorning(self) ->None:
         print("Good Morning! ",self.name)
@@ -15,3 +15,16 @@ class Greet:
         
     def goodNight(self) -> None:
         print("Good Night !",self.name)
+ 
+greet = Greet("Hari Bahadur")       
+t = int(time.strftime("%H"))
+# tm = t - 12 if t > 12 else t
+
+if t > 0 and t < 12:
+    greet.goodMorning()
+elif t > 12 and t < 17:
+    greet.goodAfternoon()
+elif t > 17 and t < 19:
+    greet.goodEvening()
+else:
+    greet.goodNight()

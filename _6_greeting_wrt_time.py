f24a4670fcc57c4fb96345abe72e7ba9e0dcc178
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-01 21:03:25 +0545

    6 greeting with respect to time added

---
+++ _6_greeting_wrt_time.py
@@ -0,0 +1,17 @@
+# Create a program to greet with respect to time
+import time
+class Greet:
+    def __init__(self,name) -> None:
+        self.name
+        
+    def goodMorning(self) ->None:
+        print("Good Morning! ",self.name)
+        
+    def goodAfternoon(self) -> None:
+        print("Good Afternoon! ",self.name)
+        
+    def goodEvening(self) -> None:
+        print("Good Evening ! ",self.name)
+        
+    def goodNight(self) -> None:
+        print("Good Night !",self.name)

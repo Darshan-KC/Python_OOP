e75f24b20bbba840a452418caa5e3cebf1502ace
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-02 20:54:01 +0545

    7 encapsulation complete

--- _7_encapsulation.py
+++ _7_encapsulation.py
@@ -7,6 +7,14 @@ class MyClass:
 
 class Derived(MyClass):
     pass
-
+print("\nBase class\n")
 obj = MyClass()
 print(obj.public)
+print(obj._protected)
+
+# This cannot be access
+# print(obj.__private)
+print("\n\nDerived class \n")
+der = Derived()
+print(der.public)
+print(der._protected)

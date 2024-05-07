430ab6520f260194534b2f7b50cc01cde1140aef
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-07 22:57:18 +0545

    10 map filter reduce finish

--- _10_map_filter_reduce.py
+++ _10_map_filter_reduce.py
@@ -1,1 +1,26 @@
 # Trying map filter and reduce
+from functools import reduce
+
+# Map
+def cube(n):
+    return n**3
+
+lst = [1,2,3,4]
+print(list(map(cube,lst)))
+
+# Filter
+def filter_function(a):
+    return a>2
+
+newl = list(filter(filter_function,lst))
+print(newl)
+
+# Reduce
+
+# sum = reduce(lambda x,y: x + y, lst)
+
+def mysum(x,y):
+    return x+y 
+
+sum = reduce(mysum,lst)
+print(sum)

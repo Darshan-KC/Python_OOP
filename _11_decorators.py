b41d1465a3eafeb4bf36f382f1952bce05cb2f12
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-08 21:59:51 +0545

    11 decorator added

---
+++ _11_decorators.py
@@ -0,0 +1,4 @@
+# Decorators in python are a powerful and versatile tool that allow you to modify the behaviour or a function and method. They are way to extend the functionlity of a function or method without modifying its source code.
+
+# A decorator  is a function that takes another function as an argument and returns a new function that modifies the behaviour of the original function. 
+# The new function is often referred to as a decorated function. The basic syntax for using a decorator is the following: 

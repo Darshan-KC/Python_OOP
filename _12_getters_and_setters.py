65cf74f55231362cc61bbf54aa3397672d569ef6
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-09 21:56:27 +0545

    12 getters and setters added

---
+++ _12_getters_and_setters.py
@@ -0,0 +1,5 @@
+# Getters in python are methods that are used to access the values of an object's properties. They are used to return the value of a specific property, and are typically defined using the @property decorator.
+
+# In Conclusion, getters are convenient way to access the values of an object's properties, while keeping the internal representation of the property hidden. This can useful for encapsulation and data validation.
+
+# It is important to note that the getters do not take any parameters and we cannot set the value through getter method. For that we need setter method which can be added by decorating method with @property_name.setter

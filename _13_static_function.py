2e59caf18f4bfcc3ef612814985cab0b10f2256b
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-10 22:19:08 +0545

    13 static function complete

--- _13_static_function.py
+++ _13_static_function.py
@@ -1,1 +1,20 @@
 # Trying python static function
+
+class Math:
+    def __init__(self,num) -> None:
+        self.num = num
+
+    def add_to_num(self,num) -> None:
+        self.num +=num
+
+    @staticmethod
+    def add(a,b):
+        return a + b
+    
+mth = Math(6)
+print(mth.num)
+
+mth.add_to_num(10)
+print(mth.num)
+
+print(Math.add(4,6))

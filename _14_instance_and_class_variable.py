b56d3d94c103fd1025ed265bb55d6825edac3562
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-11 22:34:39 +0545

    14 instance and class variable added

---
+++ _14_instance_and_class_variable.py
@@ -0,0 +1,6 @@
+# Instance Vs Class Variable
+## Instance
+# In python, variable can be defined at the class level or at the instance level. 
+
+## Class Variable
+# Class variable are defined at the class level and are shared among all instances of the class. They are defined outside of any method and are usually used to store information that is common to all instances of the class.

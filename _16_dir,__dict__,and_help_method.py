926c0c28690c2023ce0237a0bc5579a198aedf99
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-13 20:59:36 +0545

    16 dir, __dict__ and help method added

---
+++ _16_dir,__dict__,and_help_method.py
@@ -0,0 +1,5 @@
+## The dir() method
+# dir(): The dir() function returns a list of all the attributes and methods (including dunder methods) available for an object. It is a useful tool for discovering what you can do with an object. 
+
+x = [1,2,3]
+print(dir(x))

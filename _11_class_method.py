ecff42d2cf03adf65ae30290ce7fb6ffe77803b0
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-14 22:12:51 +0545

    11 class method added

---
+++ _11_class_method.py
@@ -0,0 +1,2 @@
+# A class method is a type of method that is bound to the class and not the instance of the class. In other words, it operates on the class as a whole, rather than on a specific instance of class.
+# Class methods are defined using the "@classmethod" decorator, followed by a function definition. The first argument of the function is always "cls", which represents the class.

169dd8d3a453f79afdd07672add28290f37eece9
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-17 22:12:29 +0545

    19 multilevel inheritance start

---
+++ _19_multilevel_inheritance.py
@@ -0,0 +1,4 @@
+# Multilevel inheritance is a type of inheritance in object-oriented programming where a derived class inherits from another derived class. This type of inheritance allow you to build a hierarchy of classes where one class builds upon another, leading to more specialized class. 
+
+
+    

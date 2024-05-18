42a8ac2175e2caba296700696b68635fd3bd5664
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-18 22:10:05 +0545

    progressing

--- _20_hybrid_and_hierarchical_inheritance.py
+++ _20_hybrid_and_hierarchical_inheritance.py
@@ -2,3 +2,14 @@
 
 # In python, hybrid inheritance can be implement by creating a class hierarchy, in which a base class is inherited by multiple derived classes, and one of the derived classes is further inherited by a sub-derived class.
 
+class BaseClass:
+    pass
+
+class Derived1(BaseClass):
+    pass
+
+class Derived2(BaseClass):
+    pass
+
+class Derived3(Derived1,Derived2):
+    pass

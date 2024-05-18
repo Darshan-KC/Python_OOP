d89f243571e018b0852f23d22aa62850b558e706
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-18 21:53:51 +0545

    20 hybrid and hierarchical inheritance added

---
+++ _20_hybrid_and_hierarchical_inheritance.py
@@ -0,0 +1,4 @@
+# Hybrid inheritance is a combination of multiple inheritance and single inheritance in object oriented programming. It is a type of inheritance in which multiple inheritance is used to inherit the properties of multiple base classes into a single derived class, and single inheritance is used to inherit the properties of derived class into a sub-derived class.
+
+# In python, hybrid inheritance can be implement by creating a class hierarchy, in which a base class is inherited by multiple derived classes, and one of the derived classes is further inherited by a sub-derived class.
+

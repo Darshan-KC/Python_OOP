f7ef7316927d2e93bd8f200e43ebdd1c2cfd3c98
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-20 21:20:01 +0545

    progress

--- _21_time_module.py
+++ _21_time_module.py
@@ -1,1 +1,7 @@
 # Time module
+
+# The time module in python provides a set of functions to work with time related operations, such as timekeeping, formatting, and time conversions.
+
+# time.time()
+#The time.time() function returns the current time as a floating point number, representing the number of seconds since the epoch (the point in time when the time module was initialized). The returned value is based on the computer's system clock and is affected by time adjustments made by the operating system, such as daylight saving time.
+

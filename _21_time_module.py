ece3dd0fd7305749812a28b0f1363f076acdf81d
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-20 21:26:40 +0545

    21 time module complete

--- _21_time_module.py
+++ _21_time_module.py
@@ -5,3 +5,15 @@
 # time.time()
 #The time.time() function returns the current time as a floating point number, representing the number of seconds since the epoch (the point in time when the time module was initialized). The returned value is based on the computer's system clock and is affected by time adjustments made by the operating system, such as daylight saving time.
 
+import time
+
+print(time.time())
+init = time.time()
+
+print("Before sleep")
+time.sleep(2)
+print("This is printed after 2 second")
+
+t = time.localtime()
+print("Time is ",time.strftime("%H:%M:%S",t))
+

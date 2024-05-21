58710d07963219c975d27147da42ff706e37dd73
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-21 22:03:18 +0545

    progressing

--- _22_command_line_utility.py
+++ _22_command_line_utility.py
@@ -1,2 +1,6 @@
 # Command line utilities are programs that can be run from the terminal or command line interface, and they are an essential part of many development workflows.
 # In python, you can create your own command line utilities using the built-in argprase module.
+
+import argparse
+
+parser = argparse.ArgumentParser()

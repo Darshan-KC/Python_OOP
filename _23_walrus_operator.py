7f719fb3f72cb3d2f4c25283436a03770662b14f
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-23 19:29:46 +0545

    23 walrus operator added

---
+++ _23_walrus_operator.py
@@ -0,0 +1,2 @@
+# The Walrus Operator is a new addition to Python 3.8 and allows you to assign a value to a variable within a expression. This can be useful when you need to use a value multiple times in a loop, but don't want to repeat the calculation. 
+# The Walrus Operator is represented by the := syntax and can be used in a variety of contexts including while loops and if statements.

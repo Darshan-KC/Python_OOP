e35669b389cd7239695705f17c646404fbffa998
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-24 20:34:54 +0545

    24 shutil module added

---
+++ _24_shutil_module.py
@@ -0,0 +1,2 @@
+# Shutil Module in Python
+# Shutil is a python module that provides a higher level interface for working with file and directories. The name "shutil" is short for shell utility. It provides a convenient and efficient way to automate tasks that are commonly performed on files and directories. 

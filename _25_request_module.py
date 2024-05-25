02273c2d7bf682a29df6c62022e940c1bc07a589
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-25 22:17:07 +0545

    25 request module added

---
+++ _25_request_module.py
@@ -0,0 +1,3 @@
+# Request module
+
+# The request module is an HTTP library that enables the developers to send HTTP requests in Python. This module enables you to send HTTP requests using Python code and make it possible to interact with APIs and web services.

4cc1edef46a0a5088e56f617ec24be0966c76035
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-26 20:53:45 +0545

    26 news Api added

---
+++ _26_news_app.py
@@ -0,0 +1,8 @@
+# Exercise 10
+
+# Use the NewsAPI and the requests module to fetch the daily news related to different topics. 
+# Go to https://newsapi.org/ and explore the various options to build you application
+
+import requests
+
+url = "https://newsapi.org/"

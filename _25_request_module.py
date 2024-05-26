70e3afc549cd91940ea68a7c82f777995aebbbc7
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-26 20:46:15 +0545

    25 request module complete

--- _25_request_module.py
+++ _25_request_module.py
@@ -1,3 +1,23 @@
 # Request module
 
 # The request module is an HTTP library that enables the developers to send HTTP requests in Python. This module enables you to send HTTP requests using Python code and make it possible to interact with APIs and web services.
+
+import requests
+
+# response = requests.get("https://www.google.com")
+# print(response.text)
+
+data = {
+    'title' : "Test Post",
+    'body' : "This is body",
+    "userId" : 1,
+}
+
+headers = {
+    'Content-type': 'application/json; charset=UTF-8',
+}
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+response = requests.post(url,headers=headers,json=data)
+print(response.text)

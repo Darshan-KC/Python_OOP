49dab3eb9526a19519f1aec59ec8116f242819c0
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-28 21:41:34 +0545

    27 generators added

---
+++ _27_generators.py
@@ -0,0 +1,2 @@
+# Generators in Python are special type of functions that allow you to create an iterable sequence of values. A generator function returns a generator object, which can be used to generate the values one-by-one as you iterate over it. 
+# Generators are a powerful tool for working with large or complex data sets, as they allow you to generate the values on-the-fly, rather than having to create and store the entire sequence in memory. 

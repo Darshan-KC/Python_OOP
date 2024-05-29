ca7857ad41a3cb8604e9de86f3c90833acca5fc2
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-29 21:49:53 +0545

    28 function caching complete

--- _28_function_caching.py
+++ _28_function_caching.py
@@ -4,5 +4,27 @@
 
 # In Python, function caching can be achieved using the functools.lru_cache decorator. The functools.lru_cache decorator is used to cache the results of a function so that you can reuse the results instead of recomputing them every time the function is called. 
 
-import functools
-# from functools import lru_cache
+# import functools
+from functools import lru_cache
+import time
+
+# @functools.lru_cache(maxsize=None)
+@lru_cache(maxsize=None)
+def fx(n):
+    time.sleep(5)
+    return n*5
+
+print(fx(20))
+print("Done for 20")
+print(fx(2))
+print("Done for 2")
+print(fx(6))
+print("Done for 6")
+
+print("\nPrinting again\n")
+print(fx(20))
+print("Done for 20")
+print(fx(2))
+print("Done for 2")
+print(fx(6))
+print("Done for 6")

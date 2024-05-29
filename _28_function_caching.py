f95d2c8e8dfca67f4c98bef3281c01c405f286ab
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-29 21:46:23 +0545

    28 function caching added

---
+++ _28_function_caching.py
@@ -0,0 +1,8 @@
+# Function Caching in Python
+
+# Function caching is a technique for improving the performance of a program by storing the results of a function call so that you can reuse the results instead of recomputing them every time the function is called. This can be particularly useful when a function is computationally expensive, or when the inputs to the function are unlikely to change frequently.
+
+# In Python, function caching can be achieved using the functools.lru_cache decorator. The functools.lru_cache decorator is used to cache the results of a function so that you can reuse the results instead of recomputing them every time the function is called. 
+
+import functools
+# from functools import lru_cache

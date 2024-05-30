5174934fc11f72bb28eaf6be0379821eeb005419
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-30 21:49:05 +0545

    29 regular expression added

---
+++ _29_regular_expression.py
@@ -0,0 +1,3 @@
+# Regular expression in Python
+
+# Regular expressions, or regex for short are a powerful tool for working with string and text data in python. They allow you to match and manipulate strings based on patterns, making it easy to perform complex string operations with just a few lines of code.

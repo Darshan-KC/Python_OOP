4e7488d16e3506d8981fde43bb76c10dd3df6f4a
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-05-31 21:37:17 +0545

    progress

--- _30_drink_water_remainder.py
+++ _30_drink_water_remainder.py
@@ -1,1 +1,2 @@
 ## Exercies 11 - Drink water Remainder
+# Write a python program which reminds you of drinking water every hour or two. Your program can either beep or send desktop notifications for a specific operating system

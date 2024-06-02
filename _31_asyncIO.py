d9b637b5aeedff307d3ad2f67fbd21066bec33d8
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-06-02 21:20:23 +0545

    31 asyncIO added

---
+++ _31_asyncIO.py
@@ -0,0 +1,3 @@
+# Async IO in Python
+
+# Asynchronous I/O, or async for short, is a programming pattern that allows for high performance I/O operations in a concurrent and non-blocking manner. In python, async programming is achieved through the use of the asyncio module and asynchronous functions.

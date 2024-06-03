f1a06763eb1a6daa57b5383aa2afcae3542aa437
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-06-03 23:06:45 +0545

    31 asyncIO complete

--- _31_asyncIO.py
+++ _31_asyncIO.py
@@ -5,18 +5,38 @@
 import asyncio
 import time
 
-def function1():
-    time.sleep(3)
+# def function1():
+async def function1():
+    # time.sleep(1)
+    await asyncio.sleep(1)
     print("func 1")
     
-def function2():
-    time.sleep(3)
+# def function2():
+async def function2():
+    # time.sleep(1)
+    await asyncio.sleep(1)
     print("func 2")
+    return "this is function 2"
     
-def function3():
-    time.sleep(3)
+# def function3():
+async def function3():
+    # time.sleep(1)
+    await asyncio.sleep(3)
     print("func 3")
     
-function1()
-function2()
-function3()
+async def main():
+    # task = asyncio.create_task(function1())
+    # await function1()
+    # await function2()
+    # await function3()
+    L = await asyncio.gather(
+        function1(),
+        function2(),
+        function3(),
+    )
+    print(L)
+    
+asyncio.run(main())
+# function1()
+# function2()
+# function3()

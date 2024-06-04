72be7f4e440ff941b07220de28b18118863e2f1a
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-06-04 21:40:34 +0545

    progressing

--- _32_multithreading.py
+++ _32_multithreading.py
@@ -1,1 +1,2 @@
 # Multithreading
+# Multithreading is a technique in programming that allows multiple threads of execution to run concurrently within a single process. In Python, we can use the threading module to implement multithreading. In this tutorial, we will take a closer look at the threading module and its various functions and how they can be used in Python. 

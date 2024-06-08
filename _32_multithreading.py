87d88c1e38d86057ac2bd69303d97d9370b457df
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-06-08 21:14:52 +0545

    32 multithreading complete

--- _32_multithreading.py
+++ _32_multithreading.py
@@ -6,11 +6,53 @@
 
 import threading
 import time
+from concurrent.futures import ThreadPoolExecutor
 
 def func(seconds):
     print(f"Sleeping for {seconds} seconds")
     time.sleep(seconds)
+    return seconds
 
-func(4)
-func(4)
-func(4)
+def main():
+    time1 = time.perf_counter()
+
+    # func(4)
+    # func(4)
+    # func(4)
+
+    # time2 = time.perf_counter()
+    # print("Tradition time is ",(time2 - time1))
+
+
+    t1 = threading.Thread(target=func,args=[4])
+    t2 = threading.Thread(target=func,args=[4])
+    t3 = threading.Thread(target=func,args=[4])
+
+    # This will start the process and move forward
+    t1.start()
+    t2.start()
+    t3.start()
+
+    # This will wait for the process to complete e.g wait for t1 but since the process are already run in parallel. All the process will finish at once.
+    t1.join()
+    t2.join()
+    t3.join()
+
+    time2 = time.perf_counter()
+    print("Threading time is ",(time2 - time1))
+    
+def poolDemo():
+    with ThreadPoolExecutor() as executor:
+        # future1 = executor.submit(func,4)
+        # future2 = executor.submit(func,3)
+        # future3 = executor.submit(func,2)
+        # print(future1.result())
+        # print(future2.result())
+        # print(future3.result())
+        
+        l = [4,3,2]
+        results = executor.map(func,l)
+        for result in results:
+            print(result)
+
+poolDemo()

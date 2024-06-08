d5d793782328fbbce8ff1f398571c7f1fc544140
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-06-08 20:53:09 +0545

    progressing

--- _32_multithreading.py
+++ _32_multithreading.py
@@ -1,4 +1,16 @@
 # Multithreading
 # Multithreading is a technique in programming that allows multiple threads of execution to run concurrently within a single process. In Python, we can use the threading module to implement multithreading. In this tutorial, we will take a closer look at the threading module and its various functions and how they can be used in Python. 
 
-import asyncio
+# Importing Threading
+# We can use threading by importing the threading module.
+
+import threading
+import time
+
+def func(seconds):
+    print(f"Sleeping for {seconds} seconds")
+    time.sleep(seconds)
+
+func(4)
+func(4)
+func(4)

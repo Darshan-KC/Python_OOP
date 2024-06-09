1c144faaf7b84f19132ea30136b8af181275b710
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-06-09 19:31:02 +0545

    33 multiprocessing added

---
+++ _33_multiprocessing.py
@@ -0,0 +1,7 @@
+# Multiprocessing in Python
+# Multiprocessing is a python module that provides a simple way to run multiple process in parallel. It allows you to take advantage of multiple cores or processors on your system and can significantly improve the performance of your code. 
+
+# Importing Multiprocessing 
+# We can use multiprocessing by importing the multiprocessing module.
+
+#import multiprocessing
